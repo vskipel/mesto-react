d64e06190560c64cf12c12f9e6f7c418aca9c1a8
Anton Khomchenko <khomchenko.anton@yandex.ru>
2021-02-10 13:58:36 +0300

    dev backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+    // Если всё работает, консоль покажет, какой порт приложение слушает
+    console.log(`App listening on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send(
+        `<html>
+        <body>
+            <p>Ответ на сигнал из далёкого космоса</p>
+        </body>
+        </html>`
+    );
+}); 
